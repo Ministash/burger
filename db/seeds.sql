d0bf19d2646cab45ddd4676daaaf445309f4ac2b
Bob Ross <ministash14@gmail.com>
2018-08-11 17:32:46 -0700

    Finished making the burger apps functionality

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('McMac', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('The ol ship shap mcMac patty wack burger slap', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Veggie Burger', false);
